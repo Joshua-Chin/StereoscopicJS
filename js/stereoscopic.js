958cc370e8279bfc7c98baaa33bfda088955d8e3
Joshua Chin <joshuarchin@gmail.com>
2014-03-10 19:38:51 -0400

    works

--- js/stereoscopic.js
+++ js/stereoscopic.js
@@ -1,27 +1,73 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer({antialias:true});
+window.onload = function() {
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+	var canvas = document.getElementById("canvas");
+	var hiddenCanvas = document.getElementById("hiddenCanvas");
+	var hiddenCanvas1 = document.getElementById("hiddenCanvas1");
+	var ctx = canvas.getContext("2d");
+	var hiddenCtx1 = hiddenCanvas1.getContext("2d");
+	
+	var renderer = new THREE.WebGLRenderer({
+		canvas : hiddenCanvas,
+		antialias : true
+	});
 
-var geometry = new THREE.CubeGeometry(1, 1, 1);
-var material = new THREE.MeshPhongMaterial({
-	color : 0xff0000
-});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(75, hiddenCanvas.width / hiddenCanvas.height, 0.1, 1000);
 
-var light = new THREE.DirectionalLight(0xffffff);
-light.position.set(0,3,7);
-scene.add(light);
+	var cube = new THREE.TorusGeometry(1, 0.4, 80, 60);
 
-camera.position.z = 5;
+	var white = new THREE.MeshPhongMaterial({
+		color : 0xffffff
+	});
+	var red = new THREE.MeshPhongMaterial({
+		color : 0xff0000
+	});
+	var green = new THREE.MeshPhongMaterial({
+		color : 0x00ff00
+	});
+	var blue = new THREE.MeshPhongMaterial({
+		color : 0x0000ff
+	});
+	var redCube = new THREE.Mesh(cube, white);
+	var greenCube = new THREE.Mesh(cube, white);
+	var blueCube = new THREE.Mesh(cube, white);
 
-var render = function() {
-	requestAnimationFrame(render);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	renderer.render(scene, camera);
+	redCube.position.set(0, 0, 2);
+	greenCube.position.set(2, 0, 0);
+	blueCube.position.set(-2, 5, -20);
+
+	scene.add(redCube);
+	scene.add(greenCube);
+	scene.add(blueCube);
+
+	var light = new THREE.DirectionalLight(0xffffff);
+	light.position.set(0, 3, 7);
+	scene.add(light);
+
+	camera.position.z = 10;
+
+	var render = function() {
+		requestAnimationFrame(render);
+		
+		redCube.rotation.x += 0.01;
+		redCube.rotation.y += 0.01;
+		blueCube.rotation.x += 0.01;
+		blueCube.rotation.y += 0.01;
+		greenCube.rotation.x += 0.01;
+		greenCube.rotation.y += 0.01;
+		
+		camera.position.x = -1.5;
+		light.color.setHex(0xff0000);
+		renderer.render(scene, camera);
+		hiddenCtx1.drawImage(hiddenCanvas, 0, 0);
+		
+		camera.position.x = 1.5;
+		light.color.setHex(0x00ffff);
+		renderer.render(scene, camera);
+		ctx.globalCompositeOperation = "copy";
+		ctx.drawImage(hiddenCanvas1, 0, 0);
+		ctx.globalCompositeOperation = "lighter";
+		ctx.drawImage(hiddenCanvas, 0, 0);
+	};
+	render();
 };
-render();
